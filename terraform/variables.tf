50a95ae34e55430dd2d3649802b5aca42a66352f
Nithin Katukoori <katukoorinithin@gmail.com>
2025-07-06 10:20:41 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket for static website hosting"
+  type        = string
+  default     = "my-unique-nithin-static-page-finalxyz"
+}
