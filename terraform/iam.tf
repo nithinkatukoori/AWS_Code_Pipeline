8f793cc6c6fcf9f6b501d519e2277142ef766366
Nithin Katukoori <katukoorinithin@gmail.com>
2025-07-06 22:02:25 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,44 +1,44 @@
-resource "aws_iam_role" "s3_hosting_codepipeline_role" {
-  name = "codepipeline-role-s3-hosting-2397710"
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-role-for-s3-deployment"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        Effect = "Allow",
-        Principal = {
-          Service = "codepipeline.amazonaws.com"
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "codepipeline.amazonaws.com"
         },
-        Action = "sts:AssumeRole"
+        "Action": "sts:AssumeRole"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy" "s3_hosting_codepipeline_policy" {
-  name = "codepipeline-s3-access-policy-2397710"
-  role = aws_iam_role.s3_hosting_codepipeline_role.id
+resource "aws_iam_role_policy" "codepipeline_s3_policy" {
+  name = "codepipeline-s3-access-policy"
+  role = aws_iam_role.codepipeline_role.id
 
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        Effect = "Allow",
-        Action = [
+        "Effect": "Allow",
+        "Action": [
           "s3:GetObject",
           "s3:PutObject",
           "s3:PutObjectAcl",
           "s3:ListBucket",
           "s3:DeleteObject"
         ],
-        Resource = [
-          "${aws_s3_bucket.s3_hosting_bucket.arn}",
-          "${aws_s3_bucket.s3_hosting_bucket.arn}/*"
+        "Resource": [
+          "${aws_s3_bucket.static_website_bucket.arn}",
+          "${aws_s3_bucket.static_website_bucket.arn}/*"
         ]
       },
       {
-        Effect = "Allow",
-        Action = [
+        "Effect": "Allow",
+        "Action": [
           "codedeploy:CreateDeployment",
           "codedeploy:GetApplication",
           "codedeploy:GetDeployment",
@@ -49,69 +49,71 @@ resource "aws_iam_role_policy" "s3_hosting_codepipeline_policy" {
           "codedeploy:ListDeployments",
           "codedeploy:ListApplicationRevisions"
         ],
-        Resource = "*"
+        "Resource": "*"
       },
       {
-        Effect = "Allow",
-        Action = [
+        "Effect": "Allow",
+        "Action": [
           "codebuild:StartBuild",
           "codebuild:BatchGetBuilds",
           "codebuild:StopBuild"
         ],
-        Resource = "*"
+        "Resource": "*"
       }
     ]
   })
 }
 
 
-resource "aws_iam_role" "s3_hosting_codebuild_role" {
-  name = "codebuild-role-s3-hosting-2397710"
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-role-for-static-website"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        Effect = "Allow",
-        Principal = {
-          Service = "codebuild.amazonaws.com"
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "codebuild.amazonaws.com"
         },
-        Action = "sts:AssumeRole"
+        "Action": "sts:AssumeRole"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy" "s3_hosting_codebuild_policy" {
-  name = "codebuild-s3-access-policy-2397710"
-  role = aws_iam_role.s3_hosting_codebuild_role.id
+resource "aws_iam_role_policy" "codebuild_s3_policy" {
+  name = "codebuild-s3-access-policy"
+  role = aws_iam_role.codebuild_role.id
 
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        Effect = "Allow",
-        Action = [
+        "Effect": "Allow",
+        "Action": [
           "s3:GetObject",
           "s3:PutObject",
           "s3:PutObjectAcl",
           "s3:ListBucket",
           "s3:DeleteObject"
         ],
-        Resource = [
-          "${aws_s3_bucket.s3_hosting_bucket.arn}",
-          "${aws_s3_bucket.s3_hosting_bucket.arn}/*"
+        "Resource": [
+          "${aws_s3_bucket.static_website_bucket.arn}",
+          "${aws_s3_bucket.static_website_bucket.arn}/*"
         ]
       },
       {
-        Effect = "Allow",
-        Action = [
+        "Effect": "Allow",
+        "Action": [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents"
         ],
-        Resource = "arn:aws:logs:*:*:*"
+        "Resource": "arn:aws:logs:*:*:*"
       }
     ]
   })
 }
+
